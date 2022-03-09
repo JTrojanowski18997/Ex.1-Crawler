d16b1453e16b6cafa59f6b915360a4a159c80804
Jakub Trojanowski <s18997@pjwstk.edu.pl>
2022-03-09 18:01:11 +0100

    Added argument checks

--- Ex.1-Crawler/ConsoleApp1/Crawler.cs
+++ Ex.1-Crawler/ConsoleApp1/Crawler.cs
@@ -9,9 +9,21 @@ namespace Crawler
 	{
 		public async static Task Main(string[] args)
 		{
+			// Check if argument was given
+			if (args.Length == 0)
+			{
+				throw new ArgumentNullException();
+			}
 			string website = args[0];
+
+			// Check if argument is a proper URL
+			Regex URLregex = new Regex(@"https?:\/\/(www\.)?[-a-zA-Z0-9@:%._\+~#=]+\.[a-zA-Z0-9()]+\b([-a-zA-Z0-9()@:%_\+.~#?&//=]*)");
+			if (!URLregex.IsMatch(website))
+            {
+				throw new ArgumentException();
+            }
+
 			HttpClient httpClient = new HttpClient();
-			
 			HttpResponseMessage response = await httpClient.GetAsync(website);
 			string body = await response.Content.ReadAsStringAsync();
 
@@ -23,6 +35,9 @@ namespace Crawler
 			{
 				Console.WriteLine(item);
 			}
+
+			httpClient.Dispose();
+			response.Dispose();
 		}
 	}
 }
